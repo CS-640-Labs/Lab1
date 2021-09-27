c7d21af1a5f4e1d0b00f575e68f03613ff34894c
Gage Krumbach <gkrumbach@gmail.com>
2021-09-27 18:10:33 -0500

    client fix

--- src/Client.java
+++ src/Client.java
@@ -4,12 +4,10 @@ import java.time.Instant;
 
 public class Client{
     private Socket socket            = null;
-    // private DataInputStream  input   = null;
     private DataOutputStream out     = null;
     public  Client(String address, int port,int time){
         try{
             int bytesSent = 0;
-            // System.out.println(x);
             long time_now=0;
             socket = new Socket(address, port);
             out    = new DataOutputStream(socket.getOutputStream());
@@ -24,9 +22,6 @@ public class Client{
                 }
                 out.flush();
             }
-            System.out.println(Instant.now().getEpochSecond()-time_now);
-            // System.out.print("the rate is");
-            System.out.println(time);
             System.out.println("rate=" + ((8 * bytesSent) / (time*1000000.0)) + " Mbps");
             System.out.println("Sent=" + bytesSent / 1000 + " KB");
             out.close();
